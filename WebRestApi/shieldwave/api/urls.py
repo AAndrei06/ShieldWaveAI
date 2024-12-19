9faa6705b3aa77e523f49a749c9e594197d27aad
AAndrei06 <andreiarsenii90@gmail.com>
2024-12-19 19:54:19 +0200

    Added deactivate camera

--- WebRestApi/shieldwave/api/urls.py
+++ WebRestApi/shieldwave/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UploadPredictionView
+from .views import UploadPredictionView, GetDeactivateInfo
 
 urlpatterns = [
     path('upload/', UploadPredictionView.as_view(), name='upload_prediction'),
+    path('deactivate/',GetDeactivateInfo.as_view(), name='deactivate_info')
 ]
