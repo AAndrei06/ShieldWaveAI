e28bebcfe60d1ae1efc1bcc05f197edf47ef0148
AAndrei06 <andreiarsenii90@gmail.com>
2025-04-06 18:47:31 +0300

    Added functions for activate/deactivate

--- WebRestApi/shieldwave/api/urls.py
+++ WebRestApi/shieldwave/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import UploadPredictionView, GetDeactivateInfo, GetCameraDeactivateInfo, GetMicDeactivateInfo, InitialCleanup
+from .views import UploadPredictionView, GetDeactivateInfo, GetCameraDeactivateInfo, GetMicDeactivateInfo, InitialCleanup, GetActivateInfo, ActivityInfo
 
 urlpatterns = [
     path('upload/', UploadPredictionView.as_view(), name='upload_prediction'),
     path('deactivate/',GetDeactivateInfo.as_view(), name='deactivate_info'),
     path('deactivate_cam/',GetCameraDeactivateInfo.as_view(), name='deactivate_cam'),
     path('deactivate_mic/',GetMicDeactivateInfo.as_view(), name='deactivate_mic'),
-    path('initial_clean/',InitialCleanup.as_view(), name='initial_cleanup')
+    path('initial_clean/',InitialCleanup.as_view(), name='initial_cleanup'),
+    path('activate/',GetActivateInfo.as_view(), name='activate'),
+    path('activity_info/',ActivityInfo.as_view(), name="active_info")
 ]
